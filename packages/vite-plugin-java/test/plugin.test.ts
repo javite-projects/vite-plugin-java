368c39e8c8383825ed4a4995c98a815dd80e03c1
Benny Yen <benny123tw@gmail.com>
2024-06-03 17:04:44 +0800

    test: add unit tests for vite-plugin-java and utils
    - Added unit tests for the `java` function in `plugin.test.ts`.
      - Tests include single input, multiple inputs, Rollup input configuration, full configuration, and user build configurations.
    - Added unit tests for the `createRollupInputConfig` function in `utils.test.ts`.
      - Tests include finding entry files and handling `cwd` setup.

--- packages/vite-plugin-java/test/plugin.test.ts
+++ packages/vite-plugin-java/test/plugin.test.ts
@@ -15,4 +15,87 @@ describe('vite-plugin-java', () => {
     expect(() => java({}))
       .toThrowError(`${PLUGIN_NAME}: missing configuration for "input".`)
   })
+
+  it('accepts a single input', () => {
+    const plugin = java('resources/js/app.ts')[0]
+
+    const config = plugin.config({}, { command: 'build', mode: 'production' })
+    expect(config.build?.rollupOptions?.input).toBe('resources/js/app.ts')
+  })
+
+  it('accepts an array of inputs', () => {
+    const plugin = java([
+      'resources/js/app.ts',
+      'resources/js/other.js',
+    ])[0]
+
+    const config = plugin.config({}, { command: 'build', mode: 'production' })
+    expect(config.build?.rollupOptions?.input).toEqual(['resources/js/app.ts', 'resources/js/other.js'])
+  })
+
+  it('accepts a Rollup input configuration', () => {
+    const plugin = java({
+      input: {
+        app: 'resources/js/app.ts',
+        other: 'resources/js/other.js',
+      },
+    })[0]
+
+    const config = plugin.config({}, { command: 'build', mode: 'production' })
+    expect(config.build?.rollupOptions?.input).toEqual({
+      app: 'resources/js/app.ts',
+      other: 'resources/js/other.js',
+    })
+  })
+
+  it('accepts a full configuration', () => {
+    const plugin = java({
+      input: 'resources/js/app.ts',
+      publicDirectory: 'other-public',
+      buildDirectory: 'other-build',
+      outputDirectory: 'other-output',
+    })[0]
+
+    const config = plugin.config({}, { command: 'build', mode: 'production' })
+    expect(config.base).toBe('/other-build/')
+    expect(config.build?.manifest).toBe('manifest.json')
+    expect(config.build?.outDir).toBe('other-output')
+    expect(config.build?.rollupOptions?.input).toBe('resources/js/app.ts')
+  })
+
+  it('respects the users build.manifest config option', () => {
+    const plugin = java({
+      input: 'resources/js/app.js',
+    })[0]
+
+    const userConfig = { build: { manifest: 'my-custom-manifest.json' } }
+
+    const config = plugin.config(userConfig, { command: 'build', mode: 'production' })
+
+    expect(config.build?.manifest).toBe('my-custom-manifest.json')
+  })
+
+  it('has a default manifest path', () => {
+    const plugin = java({
+      input: 'resources/js/app.js',
+    })[0]
+
+    const userConfig = {}
+
+    const config = plugin.config(userConfig, { command: 'build', mode: 'production' })
+
+    expect(config.build?.manifest).toBe('manifest.json')
+  })
+
+  it('respects users base config option', () => {
+    const plugin = java({
+      input: 'resources/js/app.ts',
+    })[0]
+
+    const userConfig = { base: '/foo/' }
+
+    const config = plugin.config(userConfig, { command: 'build', mode: 'production' })
+
+    expect(config.base).toBe('/foo/')
+  })
 })
---
+++ packages/vite-plugin-java/test/utils.test.ts
@@ -0,0 +1,51 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createRollupInputConfig } from '../src/utils'
+
+const files = [
+  'src/main.ts',
+  'src/other.ts',
+  'src/another.ts',
+  'src/nested/main.ts',
+  'src/nested/other.ts',
+]
+
+function getAbsolutePath(filepath: string) {
+  return path.normalize(path.resolve(process.cwd(), filepath))
+}
+
+describe('vite-plugin-java - utils', () => {
+  beforeAll(() => {
+    for (const file of files) {
+      const filePath = path.join('fixtures', file)
+      fs.mkdirSync(path.dirname(filePath), { recursive: true })
+      fs.writeFileSync(filePath, '/** I love Java! **/')
+    }
+  })
+
+  afterAll(() => {
+    fs.rmSync('fixtures', { recursive: true, force: true })
+  })
+
+  it('should find all the entry files', () => {
+    const inputs = createRollupInputConfig('fixtures/src/**/*.ts', 'fixtures/src')
+
+    expect(inputs).toEqual({
+      main: getAbsolutePath('fixtures/src/main.ts'),
+      other: getAbsolutePath('fixtures/src/other.ts'),
+      another: getAbsolutePath('fixtures/src/another.ts'),
+      [path.normalize('nested/main')]: getAbsolutePath('fixtures/src/nested/main.ts'),
+      [path.normalize('nested/other')]: getAbsolutePath('fixtures/src/nested/other.ts'),
+    })
+  })
+
+  it('should find all the entry files with cwd setup', () => {
+    const inputs = createRollupInputConfig('src/**/main.ts', 'src', { cwd: 'fixtures' })
+
+    expect(inputs).toEqual({
+      main: getAbsolutePath('src/main.ts'),
+      [path.normalize('nested/main')]: getAbsolutePath('src/nested/main.ts'),
+    })
+  })
+})
